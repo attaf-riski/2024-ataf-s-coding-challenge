1e3243169c8285380b7b9c6025556c3d7f4cf2e5
attaf-riski <atafriski27@gmail.com>
2024-02-02 22:52:36 +0700

    + add d1 c# odd even checker

---
+++ c#/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+
+public class OddEven
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(evenOrOdd(2));
+    }
+
+    public static string evenOrOdd(int number)
+    {
+        return (number % 2 == 0) ? "Even" : "Odd";
+    }
+}
