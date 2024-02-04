84156d61f5e6e6c4a4335e73816470dcf5111367
attaf-riski <atafriski27@gmail.com>
2024-02-04 08:22:50 +0700

    d3 is he gonna survive chal js

--- c#/obj/Debug/net8.0/c#.AssemblyInfo.cs
+++ c#/obj/Debug/net8.0/c#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3335d974151ee3e71fec47e28ef8ceda225a8fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fda6ab218b4d5f59d7aac33f0b5d1ceb1574c8c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ js/3_is_he_gonna_survive.js
@@ -0,0 +1,5 @@
+// first shot
+function hero(bullets, dragons){
+    return (dragons*2<=bullets)?true:false;
+}
+  
