71b71f79a771b06f5fb20221784d535545e480fd
attaf-riski <atafriski27@gmail.com>
2024-02-05 06:31:24 +0700

    + d4 add chal sentence smash

---
+++ js/4_sentence_smash.js
@@ -0,0 +1,8 @@
+// 4/366
+// https://www.codewars.com/kata/53dc23c68a0c93699800041d/train/javascript
+
+function smash (words) {
+    return words.join(' ');
+ };
+
+ console.log(smash(['kimo','kemi','kuma']))
