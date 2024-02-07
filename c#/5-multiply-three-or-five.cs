c461ed90565aa6d4e28f51839d0a702296057690
attaf-riski <atafriski27@gmail.com>
2024-02-07 09:33:21 +0700

    + add chal js day 6

--- c#/5-multiply-three-or-five.cs
+++ c#/5-multiply-three-or-five.cs
@@ -1,7 +1,31 @@
+// // https://www.codewars.com/kata/514b92a657cdc65150000006/train/csharp
+
+// using System;
+
 // public static class Kata
 // {
-//   public static int Solution(int value)
-//   {
-//     return 0;
-//   }
+
+//     public static void Main(string[] args)
+//     {
+//         Console.WriteLine(ThreeOrFive(10));
+//         Console.WriteLine(ThreeOrFive(20));
+//         Console.WriteLine(ThreeOrFive(200));
+//         Console.WriteLine(ThreeOrFive(0));
+//     }
+
+//     public static int ThreeOrFive(int number)
+//     {
+//         if (number <= 0)
+//         {
+//             return 0;
+//         }
+
+//         int buffer = 0;
+//         for(int i = 3;i<number;i++){
+//             if(i % 3 == 0 || i % 5 == 0){
+//                 buffer += i;
+//             }
+//         }
+//         return buffer;
+//     }
 // }
