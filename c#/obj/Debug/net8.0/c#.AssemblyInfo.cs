51702e2b153cca68ee151d985f5a341fc2b45a06
attaf-riski <atafriski27@gmail.com>
2024-02-19 11:19:56 +0700

    add chal c# d15

--- c#/obj/Debug/net8.0/c#.AssemblyInfo.cs
+++ c#/obj/Debug/net8.0/c#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a3b807545d15ec9c0ccec0e510f0cb6442da7cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68b52a2e0aaafdb3e912c6c82f5e7f748a890330")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
