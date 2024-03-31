156a5b9ffe1e4df07148e2308adbeaeb9d807d14
attaf-riski <atafriski27@gmail.com>
2024-03-31 22:27:38 +0700

    add chal d26 js

--- c#/obj/Debug/net8.0/c#.AssemblyInfo.cs
+++ c#/obj/Debug/net8.0/c#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2418dfe29ff1333db9e913caba05bb48dab7e155")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e3d70492b4b6209b4aa7831e9823bc23b2abbce")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
